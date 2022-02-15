0839057506cd93673eca47b80c10488762909bc9
Toyotomi <dragonisbornsmall09@hotmail.com>
2022-02-15 20:01:45 +0000

    added chakra ui and intial signup component

--- apps/gladiator/Controllers/WeatherForecastController.cs
+++ apps/gladiator/Controllers/WeatherForecastController.cs
@@ -29,4 +29,16 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+        [HttpPost(Name = "hello")]
+    public IEnumerable<WeatherForecast> post()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
