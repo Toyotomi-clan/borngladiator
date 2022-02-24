4abb6b401ab1fe427ead78094abf3852cf3765a3
Toyotomi <dragonisbornsmall09@hotmail.com>
2022-02-24 22:56:09 +0000

    completed the happy path for loggin, will need to consider load, network and post errors next

--- apps/born/src/app/login.tsx
+++ apps/born/src/app/login.tsx
@@ -10,23 +10,23 @@ import {
   Button,
   Heading,
   Text,
-  useColorModeValue,
+  useColorModeValue, FormErrorMessage,
 } from '@chakra-ui/react';
 
 import {Link as ReactRouterLink} from "react-router-dom";
 
-import useStartLoginFlow, {postLoginForm, useMutateLogin} from "./Api/Api";
+import useStartLoginFlow, {useMutateLogin} from "./Api/Api";
 import {useForm} from "react-hook-form";
 import {LoginFormModel} from "./models/registerFormModel";
 import {SubmitSelfServiceLoginFlowBody} from "@ory/client/dist/api";
 import {findCsrfToken} from "./helper/oryHelper";
-import {SelfServiceLoginFlow} from "@ory/client";
+import {errorIsValid} from "./helper/EmptyObjectHelper";
 
 
 export default function Login() {
 
   const { status, data, error , isFetching } = useStartLoginFlow();
-  const {handleSubmit, setError,register}  = useForm<LoginFormModel>();
+  const {handleSubmit, setError,register, formState: {errors}}  = useForm<LoginFormModel>();
   const mutation  = useMutateLogin();
 
   console.log({status,data,error,isFetching})
@@ -65,13 +65,19 @@ export default function Login() {
           boxShadow={'lg'}
           p={8}>
           <Stack spacing={4}>
-            <FormControl id="email">
+            <FormControl id="email"  isInvalid={errorIsValid(errors,errors.email)}>
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input type="email" {...register("email")}/>
             </FormControl>
-            <FormControl id="password">
+            <FormControl id="password"  isInvalid={errorIsValid(errors,errors.password)}>
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
+              <Input type="password" {...register("password")}/>
+            </FormControl>
+
+            <FormControl isInvalid={errorIsValid(errors,errors.oryValidationGeneral)}>
+              <Text {...register("oryValidationGeneral",{required: false})}/>
+
+              <FormErrorMessage>{errors.oryValidationGeneral?.message}</FormErrorMessage>
             </FormControl>
             <Stack spacing={10}>
               <Stack
@@ -82,6 +88,7 @@ export default function Login() {
                 <Link color={'blue.400'}>Forgot password?</Link>
               </Stack>
               <Button
+                type={"submit"}
                 bg={'blue.400'}
                 color={'white'}
                 _hover={{
