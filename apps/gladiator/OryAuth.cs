786d35a7be397af09c6ce2e5cf21e13eebac5e4b
Toyotomi <dragonisbornsmall09@hotmail.com>
2022-04-11 17:07:42 +0100

    added ory authentication layer to the back end

---
+++ apps/gladiator/OryAuth.cs
@@ -0,0 +1,184 @@
+﻿using System.ComponentModel;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using Ory.Client.Api;
+using Ory.Client.Client;
+
+namespace Borngladiator.Gladiator;
+
+public class OryAuthOptions : AuthenticationSchemeOptions
+{
+  public string? EndPoint { get; set; }
+
+  public string? Realm { get; set; }
+
+  public string? Slug { get; set; }
+
+  public ITicketStore? SessionStore { get; set; }
+
+}
+
+public class OryAuthOptionsConfiguration : IPostConfigureOptions<OryAuthOptions>
+{
+  public void PostConfigure(string name, OryAuthOptions options)
+  {
+    if (string.IsNullOrWhiteSpace(options.Realm))
+    {
+      throw new InvalidOperationException("A realm is required to specified");
+    }
+    if(string.IsNullOrWhiteSpace(options.EndPoint))
+    {
+      throw new InvalidOperationException(
+        "Ory endpoint must be specified (i.e. https://{your cloud instance}.projects.oryapis.com)");
+    }
+
+    if (string.IsNullOrWhiteSpace(options.Slug))
+    {
+      throw new InvalidOperationException("A ory slug is required to specified i.e. playboy-carti-k1hdi6xth0	");
+    }
+  }
+}
+
+public class OryAuthHandler : AuthenticationHandler<OryAuthOptions>
+{
+  private V0alpha2Api _alpha2;
+  private IMemoryCache _memoryCache;
+  public OryAuthHandler(IOptionsMonitor<OryAuthOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, V0alpha2Api alpha2, IMemoryCache memoryCache) : base(options, logger, encoder, clock)
+  {
+    _alpha2 = alpha2;
+    _memoryCache = memoryCache;
+  }
+
+  protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+  {
+
+    try
+    {
+
+      var slugTrimmed = Options.Slug?.Replace("-", "").Trim();
+
+      if (slugTrimmed == null)
+      {
+        //Todo: log this
+        return AuthenticateResult.NoResult();
+      }
+      var orySession = $"ory_session_{slugTrimmed}";
+
+      var sessionCookie = Request.Cookies[orySession];
+
+      if (sessionCookie == null)
+      {
+        return AuthenticateResult.NoResult();
+      }
+
+      var oryCookie = $"{orySession}={sessionCookie}";
+
+      if (_memoryCache.TryGetValue(oryCookie, out AuthenticationTicket alreadyAuthenticated) &&
+          DateTime.Compare( alreadyAuthenticated.Properties.ExpiresUtc?.UtcDateTime ?? DateTime.UtcNow,DateTime.UtcNow) > 0 )
+      {
+        return AuthenticateResult.Success(alreadyAuthenticated);
+      }
+
+      var user =  await _alpha2.ToSessionAsync(null,oryCookie);
+
+      if (user == null)
+      {
+        //Todo: log this
+        return AuthenticateResult.NoResult();
+      }
+
+      var jsonTraits = user.Identity.Traits as JObject;
+
+      var traits = jsonTraits?.ToObject<UserTraits>();
+
+      if (traits == null)
+      {
+        //Todo: log this
+        return AuthenticateResult.NoResult();
+      }
+
+      var claims = new[]
+      {
+        new Claim(ClaimTypes.Sid, user.Id),
+        new Claim(ClaimsIdentity.DefaultIssuer, "Ory_Cloud"),
+        new Claim(ClaimTypes.Name, traits.UserName),
+        new Claim(ClaimTypes.Email, traits.Email)
+      };
+
+      var identity = new ClaimsIdentity(claims, Scheme.Name);
+
+      var principle = new ClaimsPrincipal(identity);
+
+      var ticket = new AuthenticationTicket(principle,new AuthenticationProperties
+      {
+        ExpiresUtc = user.ExpiresAt,
+        AllowRefresh = false,
+        IsPersistent = true,
+        IssuedUtc = user.IssuedAt,
+      }, Scheme.Name);
+
+      _memoryCache.Set(oryCookie, ticket,user.ExpiresAt);
+
+      return AuthenticateResult.Success(ticket);
+    }
+    catch (Exception e)
+    {
+      //Todo Log error
+
+      return AuthenticateResult.Fail(e);
+    }
+  }
+}
+
+public class UserTraits
+{
+  [JsonPropertyName("username")]
+  public string UserName { get; set; }
+
+  [JsonPropertyName("email")]
+  public string Email { get; set; }
+}
+public static class OryAuthenticationDefaults
+{
+  public const string AuthenticationScheme = "OryAuth";
+}
+
+public static class OryAuthenticationExtensions
+{
+  public static AuthenticationBuilder AddOry(this AuthenticationBuilder builder)
+
+  {
+    return AddOry(builder, OryAuthenticationDefaults.AuthenticationScheme, _ => { });
+  }
+
+  public static AuthenticationBuilder AddOry(this AuthenticationBuilder builder, string authenticationScheme)
+  {
+    return AddOry(builder, authenticationScheme, _ => { });
+  }
+
+  public static AuthenticationBuilder AddOry(this AuthenticationBuilder builder, Action<OryAuthOptions> configureOptions)
+  {
+    return AddOry(builder, OryAuthenticationDefaults.AuthenticationScheme, configureOptions);
+  }
+
+  public static AuthenticationBuilder AddOry(this AuthenticationBuilder builder, string authenticationScheme, Action<OryAuthOptions> configureOptions)
+  {
+    builder.Services.AddSingleton<IPostConfigureOptions<OryAuthOptions>, OryAuthOptionsConfiguration>();
+    builder.Services.AddSingleton(ory => new V0alpha2Api( new Configuration
+    {
+      BasePath =  new Uri("https://competent-benz-v5hxi6dzh2.projects.oryapis.com", UriKind.Absolute).ToString()
+    }));
+
+    return builder.AddScheme<OryAuthOptions, OryAuthHandler>(
+      authenticationScheme, configureOptions);
+  }
+}
--- apps/gladiator/Program.cs
+++ apps/gladiator/Program.cs
@@ -1,3 +1,13 @@
+using System.Net.Http.Headers;
+using System.Text.Encodings.Web;
+using Borngladiator.Gladiator;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,36 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowLocalHostCORS", policyBuilder => policyBuilder
+    .WithOrigins("http://localhost:4000")
+    .WithOrigins("http://localhost:4200")
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials());
+});
+
+builder.Services.AddAuthentication(OryAuthenticationDefaults.AuthenticationScheme).AddOry(o =>
+{
+  o.Realm = "my realm hello!";
+  o.EndPoint = "lol for bants";
+  o.Slug = "competent-benz-v5hxi6dzh2";
+});
+
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddAuthorization(options =>
+{
+  options.FallbackPolicy = new AuthorizationPolicyBuilder()
+    .AddAuthenticationSchemes(OryAuthenticationDefaults.AuthenticationScheme)
+    .RequireAuthenticatedUser()
+    .Build();
+});
+
 
 var app = builder.Build();
 
@@ -16,10 +56,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//Todo: dev local cleanup
+//Todo: make react and back end utilise https
+//Currently if 1 utilises the http and other https (.net app) it won't be considered to be part of the same origin
+//app.UseHttpsRedirection();
 
+app.UseCors("AllowLocalHostCORS");
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
