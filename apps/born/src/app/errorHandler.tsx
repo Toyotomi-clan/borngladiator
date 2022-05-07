9a59c22b78a6606582961ce18111e2880faede02
Toyotomi <dragonisbornsmall09@hotmail.com>
2022-05-07 17:49:21 +0100

    We enabled tracking of authenticated users as well.

--- apps/born/src/app/errorHandler.tsx
+++ apps/born/src/app/errorHandler.tsx
@@ -1,15 +1,20 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import {environment} from "../environments/environment";
 import FingerprintJS from "@fingerprintjs/fingerprintjs"
+import {queryCache, queryClient} from "./QueryClient";
+import {QueryClient} from "react-query";
+import {Session} from "@ory/client/dist/api";
 
 export const errorHandler = async (error: Error, info: {componentStack: string}) => {
   //Get user fingerprint
   const fingerPrint =  await visitorFingerPrint();
+  const actualUser = await getAuthenticatedUser();
   //https://docs.datalust.co/docs/built-in-properties-and-functions
   const time = '@t';
   const message = "@m"
   const level = "@l"
   //Todo: maybe include device fingerprint
+
   const seqLogReport = JSON.stringify(
     {
       Exception: error,
@@ -18,7 +23,8 @@ export const errorHandler = async (error: Error, info: {componentStack: string})
       http: window.location.href,
       [message]: error.message,
       [level]: "error",
-      ...fingerPrint
+      ...fingerPrint,
+      ...actualUser
     });
 
   await axios.post(`${environment.seqLogServer}/api/events/raw?clef`,seqLogReport,{
@@ -33,5 +39,20 @@ async function visitorFingerPrint(){
      fingerPrintConfidence: fingerPrint.confidence,
      fingerPrintVisitorId: fingerPrint.visitorId,
      fingerPrintVersion: fingerPrint.version
+  }
+}
+async function getAuthenticatedUser(){
+  const user: AxiosResponse<Session> =  queryClient.getQueryData("user")
+  const session  =  user?.data?.identity;
+
+  if(!session){
+    return {
+      authenticated: false
+    }
+  }
+  
+  return {
+    authenticated: true,
+    angle: session.id,
   }
 }
